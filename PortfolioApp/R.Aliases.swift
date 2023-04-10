63ca2aeafcf8a0b03edf38ef643cdf66e1f5fff2
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-04-10 11:27:00 +0300

    Fixed R.Swift

--- PortfolioApp/R.Aliases.swift
+++ PortfolioApp/R.Aliases.swift
@@ -1,6 +1,6 @@
-//import Foundation
-//import Rswift
-//
-//typealias RText = R.string.texts
+import Foundation
+import Rswift
+
+typealias RText = R.string.texts
 //typealias RImage = R.image
-//typealias RColor = R.color
+typealias RColor = R.color
