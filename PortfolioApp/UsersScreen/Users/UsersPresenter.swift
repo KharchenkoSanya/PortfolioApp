12f40a677f0621993a836862f34e8e3a809a8fa9
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-11 20:32:46 +0200

    Removed the print

--- PortfolioApp/UsersScreen/Users/UsersPresenter.swift
+++ PortfolioApp/UsersScreen/Users/UsersPresenter.swift
@@ -9,23 +9,20 @@ final class UsersPresenter {
     weak var view: UsersView?
     
     func onRefresh() {
-        print("ONREFRESH")
         view?.display(isLoading: true)
         var request = URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/users")!)
         request.httpMethod = "GET"
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, _, error in
+        let task = session.dataTask(with: request) { data, _, _ in
             guard let data = data else { return }
             
             do {
                 let result = try JSONDecoder().decode([User].self, from: data)
-                print("DECODING RESULT \(result)")
                 DispatchQueue.main.async {
                     self.view?.display(result)
                     self.view?.display(isLoading: false)
                 }
             } catch {
-                print("decoding ERROR \(error)")
                 DispatchQueue.main.async {
                     self.view?.display(isLoading: false)
                 }
