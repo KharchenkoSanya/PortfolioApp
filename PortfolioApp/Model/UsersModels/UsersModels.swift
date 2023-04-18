652da25f795241a4dd3bea6b252c8ce7e75246c4
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-04-18 10:18:31 +0200

    Moved models to separate files

--- PortfolioApp/Model/UsersModels/UsersModels.swift
+++ PortfolioApp/Model/UsersModels/UsersModels.swift
@@ -9,14 +9,3 @@ struct User: Decodable {
     let address: Address
     let company: Company
 }
-
-struct Address: Decodable {
-    let city: String
-    let street: String
-    let suite: String
-    let zipcode: String
-}
-
-struct Company: Decodable {
-    let name: String
-}
