00acbc34f6415e8783e7882ddac764cd4767cefb
Oleksandr Kharchenko <kharchenko1996oleksandr@gmail.com>
2023-07-13 09:02:42 +0200

    Removed unnecessary override

--- PortfolioApp/Posts/Views/Cell/PostsTableViewCell/PostsTableViewCell.swift
+++ PortfolioApp/Posts/Views/Cell/PostsTableViewCell/PostsTableViewCell.swift
@@ -7,10 +7,6 @@ final class PostsTableViewCell: UITableViewCell, ReuseIdentifierType {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var bodyLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
     func setup(post: PostsData) {
         titleLabel.text = post.title
         bodyLabel.text = post.body
